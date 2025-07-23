78fd5f63c339182eacc092c5044a7dffb788af72
marcus <marcuslopee@gmail.com>
2025-07-23 17:27:38 -0300

    fix: correçao nome variavel

--- src/main/java/com/app/financialTR/config/AppConstants.java
+++ src/main/java/com/app/financialTR/config/AppConstants.java
@@ -4,5 +4,5 @@ public class AppConstants {
     public static final String PAGE_NUMBER = "1";
     public static final String PAGE_SIZE = "10";
     public static final String SORT_DIRECTION = "asc";
-    public static final String SORT_TRANSACTIONS_BY = "cd_transaction";
+    public static final String SORT_TRANSACTIONS_BY = "cdTransaction";
 }
