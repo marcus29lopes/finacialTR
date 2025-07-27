184399e6abd093d80d891fdeecd1b2b4a9a01eba
marcus <marcuslopee@gmail.com>
2025-07-26 23:22:01 -0300

    feat: implementaçao roles

--- src/main/java/com/app/financialTR/controller/CategoryController.java
+++ src/main/java/com/app/financialTR/controller/CategoryController.java
@@ -6,18 +6,20 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/FTR")
+@RequestMapping(value = "/FTR")
 public class CategoryController {
 
     @Autowired
     CategoryService categoryService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping(value = "/category/save")
     public ResponseEntity<Category> saveCategory(@Valid @RequestBody Category category) {
         categoryService.addCategory(category);
